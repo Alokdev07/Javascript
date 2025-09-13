fcdccf6d699a524756470326b4ccd38f2dd45fef
Alokdev07 <alokbhuyan162@gmail.com>
2025-09-13 20:28:10 +0530

    datatypes of javascript

---
+++ basics/02_datatypes.js
@@ -0,0 +1,14 @@
+"use strict" // -> this command is use to treat the code in newer engine
+
+let name = "Alok" // -> string datatype which is in double qoute
+let logged_in = true // -> boolean which is true or false
+let age = 18 // -> datatype considerd as number
+let login_count = null // -> null = standalone value which is used in javascript as a object
+let login_state = undefined // -> undefined which is not defined
+const id = Symbol("id") // -> for uniquely identified
+let user = {
+    name : "alok",
+    age : 18
+} // -> this of data type is called as object which is most useful in javascript
+
+console.log(typeof name) // -> to find the datatype of any variables
